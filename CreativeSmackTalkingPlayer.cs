0e9c1873c13acdfabea87c2865bb42803d4493b2
shanedbutler <shanedbutler@gmail.com>
2022-09-27 11:30:45 -0400

    Complete CreativeSmackTalkingPlayer class and add

--- CreativeSmackTalkingPlayer.cs
+++ CreativeSmackTalkingPlayer.cs
@@ -1,8 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 namespace ShootingDice
 {
     // A SmackTalkingPlayer who randomly selects a taunt from a list to say to the other player
-    public class CreativeSmackTalkingPlayer
+    public class CreativeSmackTalkingPlayer : Player
     {
-
+        private List<string> taunts = new List<string>()
+        {
+            "If I were you I'd QUIT",
+            "Face me and LOSE",
+            "I bet you can't WIN",
+            "Why you ought to STEP BACK",
+            "I'm gonna roll you OUT",
+            "Dice is my LIFE"
+        };
+        private string Taunt()
+        {
+            int randomI = new Random().Next(0, taunts.Count());
+            return taunts[randomI];
+        }
+        public override int Roll()
+        {
+            Console.WriteLine($"{this.Name} says \"{Taunt()}\"");
+            return base.Roll();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace ShootingDice
 
             Console.WriteLine("-------------------");
 
-            Player player3 = new HumanPlayer();
+            Player player3 = new CreativeSmackTalkingPlayer();
             player3.Name = "Wilma";
 
             player3.Play(player2);
